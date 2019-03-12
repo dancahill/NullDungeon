14d8467b1d0b4f3961e8f82f829a94340e323759
Dan Cahill <nulllogic@gmail.com>
2019-03-11 22:13:36 -0400

    Add project files.

---
+++ Assets/TestScene1/CharacterAnimator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour
+{
+	const float locomotionAnimationSmoothTime = .1f;
+	NavMeshAgent agent;
+	Animator animator;
+
+	void Start()
+	{
+		agent = GetComponent<NavMeshAgent>();
+		animator = GetComponentInChildren<Animator>();
+		Test1Manager tm = Resources.FindObjectsOfTypeAll<Test1Manager>()[0];
+		agent.SetDestination(tm.end.transform.position);
+	}
+
+	void Update()
+	{
+		float speedPercent = agent.velocity.magnitude / agent.speed;
+		animator.SetFloat("SpeedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+		if (Input.GetMouseButtonDown(0))
+		{
+			Camera cam = Camera.main;
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			LayerMask movementMask = LayerMask.GetMask("Environment");
+			if (Physics.Raycast(ray, out hit, movementMask))
+			{
+				agent.SetDestination(hit.point);
+			}
+		}
+	}
+}
---
+++ Assets/TestScene1/Test1Manager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Test1Manager : MonoBehaviour
+{
+	public GameObject player;
+	public GameObject end;
+
+	void Awake()
+	{
+		GameObject env = GameObject.Find("Environment");
+		NavMeshSurface nms = env.AddComponent<NavMeshSurface>();
+		nms.layerMask = 1 << LayerMask.NameToLayer("Environment");
+		nms.BuildNavMesh();
+	}
+
+	private void Update()
+	{
+		Camera.main.transform.LookAt(player.transform);
+	}
+}
