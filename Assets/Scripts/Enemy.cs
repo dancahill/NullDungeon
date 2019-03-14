e3a3d61e3c2430f8f9d0c32a8cdf837c7d977533
Dan Cahill <nulllogic@gmail.com>
2019-03-14 16:45:59 -0400

    minor fixes to enemy navigation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,19 +1,16 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(EnemyAnimator))]
 public class Enemy : MonoBehaviour
 {
 	GameManager m_Manager;
-	public NavMeshAgent m_Agent;
+	//public NavMeshAgent m_Agent;
 	const float starthealth = 100;
 	float health;
 
 	void Start()
 	{
-		m_Agent = gameObject.AddComponent<NavMeshAgent>();
-		m_Agent.speed = 2.0f;
-		if (name == "Zombie") m_Agent.speed = 1.5f;
-		m_Agent.angularSpeed = 500;
 		m_Manager = GameManager.instance;
 		health = starthealth;
 	}
--- Assets/Scripts/EnemyAnimator.cs
+++ Assets/Scripts/EnemyAnimator.cs
@@ -7,23 +7,24 @@ public class EnemyAnimator : MonoBehaviour
 	GameManager m_Manager;
 	NavMeshAgent m_Agent;
 	Animator m_Animator;
-
-	GameObject start;
-	GameObject end;
+	Vector3 start;
+	Vector3 end;
 	bool movingtoend;
 
 	void Start()
 	{
-		//m_Manager = Resources.FindObjectsOfTypeAll<Test1Manager>()[0];
 		m_Manager = GameManager.instance;
-		m_Agent = GetComponent<NavMeshAgent>();
 		m_Animator = GetComponentInChildren<Animator>();
 
-		start = GameObject.Find("Start");
-		end = GameObject.Find("End");
-
+		start = transform.position;
+		end = GameObject.Find("End").transform.position;
 		movingtoend = true;
-		m_Agent.SetDestination(end.transform.position);
+
+		m_Agent = gameObject.AddComponent<NavMeshAgent>();
+		m_Agent.speed = 2.0f;
+		if (name == "Zombie") m_Agent.speed = 1.5f;
+		m_Agent.angularSpeed = 500;
+		m_Agent.SetDestination(end);
 	}
 
 	private void Update()
@@ -32,14 +33,11 @@ public class EnemyAnimator : MonoBehaviour
 		const float locomotionAnimationSmoothTime = .1f;
 		float speedPercent = m_Agent.velocity.magnitude / m_Agent.speed;
 
-		if (m_Animator)
-		{
-			m_Animator.SetFloat("SpeedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
-		}
+		if (m_Animator) m_Animator.SetFloat("SpeedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
 		if (m_Agent.remainingDistance < 0.5f)
 		{
 			movingtoend = !movingtoend;
-			m_Agent.SetDestination(movingtoend ? end.transform.position : start.transform.position);
+			m_Agent.SetDestination(movingtoend ? end : start);
 		}
 	}
 
